f6421da7421274339c31538d644c6215e152cc69
Dmitrii-Dev-Hub <>
2025-03-09 13:03:32 +0400

    нач проекта

---
+++ Image_feed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Image_feed
+//
+//  Created by 0 on 9/3/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Image_feedUITests/Image_feedUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Image_feedUITestsLaunchTests.swift
+//  Image_feedUITests
+//
+//  Created by 0 on 9/3/2025.
+//
+
+import XCTest
+
+final class Image_feedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
